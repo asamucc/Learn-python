e9b38231b93dd9a06775e6269587cfb42d7b4f9a
asamucc <614225166@qq.com>
2020-05-12 13:06:06 +0800

    first up git

---
+++ demo01.py
@@ -0,0 +1,36 @@
+
+"""
+
+print("hello word")  #单个内容
+print("你好！", 123) #多个内容
+print("hh"+"！xx") #相同类型字符串内容拼接
+print("HOLLE"*10)  #打印10次
+print(((1+2)*100-299)/2)  #数学运算结果：0.5
+print(2>3) #判断结果为：Fales
+
+
+"""
+
+"""
+
+a=float(input("请输入内容1："))  #input默认输入内容为字符串
+b=float(input("请输入内容2："))
+
+
+print(type(a))  #结果显示位<class 'str'>
+
+print("已经输入",a+b)   #此处字符串拼接
+
+
+
+"""
+
+
+
+#print(len("123456"))  #输出结果位：6
+
+
+
+a=input("①：")
+b=input("②：")
+print("长度之和",len(a)+len(b))
